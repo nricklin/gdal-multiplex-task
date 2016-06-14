88cab2d1fd57153b6ede4925d03f2973a482cf79
Nate Ricklin <nate@rickl.in>
2016-06-14 15:34:04 -0600

    update example

--- example.py
+++ example.py
@@ -6,8 +6,6 @@ task.inputs.data1 = 's3://gbd-customer-data/7b216bd9-6523-4ca9-aa3b-1d8a5994f054
 task.inputs.data2 = 's3://gbd-customer-data/7b216bd9-6523-4ca9-aa3b-1d8a5994f054/test_acomp_output/'
 
 task.inputs.command = """
-gdalbuildvrt $outdir/out.vrt $indir/data1/*.tif $indir/data2/*.tif;
-gdal_translate $outdir/out.vrt $outdir/out.tif;
 ls $indir/data1/ > $outdir/list1.txt;
 ls $indir/data2/ > $outdir/list2.txt
 """
