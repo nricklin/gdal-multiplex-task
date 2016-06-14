5183ac7b6f9e708f8f7ef808c27e7e0e701b0501
Nate Ricklin <nate@rickl.in>
2016-06-14 15:25:03 -0600

    new example

--- example.py
+++ example.py
@@ -1,17 +1,17 @@
 from gbdxtools import Interface
 gbdx = Interface()
 
-task = gbdx.Task('ricklin-gdal-test')
-task.inputs.data = 's3://gbd-customer-data/7b216bd9-6523-4ca9-aa3b-1d8a5994f054/test_acomp_output/'
-task.inputs.filetype = 'tif'
+task = gbdx.Task('gdal-cli-multiplex')
+task.inputs.data1 = 's3://gbd-customer-data/7b216bd9-6523-4ca9-aa3b-1d8a5994f054/test_acomp_output/'
+task.inputs.data2 = 's3://gbd-customer-data/7b216bd9-6523-4ca9-aa3b-1d8a5994f054/test_acomp_output/'
 
-command = """
-gdal_calc.py -A $input --A_band=2 -B $input --B_band=3 --outfile=$output \
---calc='(A.astype(float)-B)/(A.astype(float)+B)' --type=Float32
+task.inputs.command = """
+gdalbuildvrt $outdir/out.vrt $indir/data1/*.tif $indir/data2/*.tif;
+gdal_translate $outdir/out.vrt $outdir/out.tif;
+ls $indir/data1/ > $outdir/list1.txt;
+ls $indir/data2/ > $outdir/list2.txt
 """
 
-task.inputs.command = command
-
 workflow = gbdx.Workflow([task])
-#workflow.savedata(task.outputs.data, location='gdal-test-task-output3')
+workflow.savedata(task.outputs.data, location='gdal-multiplex-task-output')
 workflow.execute()
