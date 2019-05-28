be0aa1bb5c2ca90ec1b5f1a8b7ed5b85942c0c64
mgleason <michael.gleason@digitalglobe.com>
2019-05-28 16:33:32 -0600

    updating task with better error handling to ensure correct output to task stderr, messaging in workflow events, etc

--- task.py
+++ task.py
@@ -3,6 +3,7 @@ import json
 
 outdir = '/mnt/work/output/'
 indir = '/mnt/work/input/'
+out_status = '/mnt/work/status.json'
 
 input_data = json.load(open('/mnt/work/input/ports.json'))
 
@@ -17,14 +18,24 @@ print('\n')
 
 proc = subprocess.Popen([command], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 out, err = proc.communicate()
-status = proc.returncode
+returncode = proc.returncode
 
+print('STDOUT:')
 print(out.decode())
 print('\n')
 
-if status == 0:
-    print("SUCCESS: Execution of input command completed successfully.")
-else:
-    print("ERROR: Execution of input command failed.")
-    print(err.decode())
+with open(out_status, 'w') as f:
+    # Note: return code isn't enough to capture errors that happen in a compound command
+    # so use the presence of any error message as an indicator of failure
+    if returncode == 0 and err is None:
+        msg = "SUCCESS: Execution of input command completed successfully."
+        f.write(json.dumps({'status': 'success',
+                            'reason': msg}))
+    else:
+        msg = "ERROR: Execution of input command failed."
+        msg += err.decode()
+        f.write(json.dumps({'status': 'failed',
+                            'reason': msg}))
+        raise subprocess.SubprocessError(msg)
+
 
